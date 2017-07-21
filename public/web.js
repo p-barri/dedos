7dfc3d0f4d8908a3cc7e42ac2fb77a7e5f25ebde
Francisco Memoli Olmos <francisco.memoli@gmail.com>
2017-07-21 13:08:31 -0300

    Join de grupo

--- public/web.js
+++ public/web.js
@@ -14,6 +14,10 @@ $(function () {
         $('#gameId').append(data.gameId);
         $('#gameName').append(data.gameName);
     });
+    socket.on('join game', function (data) {
+        $('#usernameLogged').append(data.username);
+        $('#gameId').append(data.gameId);
+    });
     socket.on('new user', function (data) {
         $('#users').append("<tr><td>"+data.username+"</td></tr>")
     });
@@ -33,13 +37,14 @@ $(function () {
         }
     }
     joinGame = function() {
-        var usernameInput = $('#usernameInput').val();
+        debugger
+        var usernameInput = $('#usernameJoinInput').val();
         var gameidInput = $('#gameidInput').val();
         username = usernameInput.trim();
         gameId = gameidInput.trim();
 
         if (username && gameId) {
-            socket.emit('new game', {username: username, gameId: gameId});
+            socket.emit('join game', {username: username, gameId: gameId});
             $('#chat-send').removeAttr('disabled');
             $('#loginInput').hide();
             $('#userBlock').css('display', '');
