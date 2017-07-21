96afb26664f3238c26565cce56fe0849ccc95b9b
Francisco Memoli Olmos <francisco.memoli@gmail.com>
2017-07-21 12:40:02 -0300

    Alta de juego

--- public/web.js
+++ public/web.js
@@ -9,16 +9,37 @@ $(function () {
         $('#messages').append($('<li>').text(msg));
     });
 
-    socket.on('new user', function (username) {
-        $('#usernameLogged').append(username.username);
+    socket.on('new game', function (data) {
+        $('#usernameLogged').append(data.username);
+        $('#gameId').append(data.gameId);
+        $('#gameName').append(data.gameName);
     });
+    socket.on('new user', function (data) {
+        $('#users').append("<tr><td>"+data.username+"</td></tr>")
+    });
+
 
     setUsername = function() {
         var usernameInput = $('#usernameInput').val();
+        var gamenameInput = $('#gamenameInput').val();
         username = usernameInput.trim();
+        gamename = gamenameInput.trim();
 
         if (username) {
-            socket.emit('new user', username);
+            socket.emit('new game', {username: username, gamename: gamename});
+            $('#chat-send').removeAttr('disabled');
+            $('#loginInput').hide();
+            $('#userBlock').css('display', '');
+        }
+    }
+    joinGame = function() {
+        var usernameInput = $('#usernameInput').val();
+        var gamenameInput = $('#gamenameInput').val();
+        username = usernameInput.trim();
+        gamename = gamenameInput.trim();
+
+        if (username) {
+            socket.emit('new game', {username: username, gamename: gamename});
             $('#chat-send').removeAttr('disabled');
             $('#loginInput').hide();
             $('#userBlock').css('display', '');
